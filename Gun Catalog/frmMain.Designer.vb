eebc00d5fa0129fbf2fda97270ce3bc7e3a4dc05
JIMMYGOULD-PC\Jimmy <gouljm01@students.ipfw.edu>
2016-10-25 23:25:54 -0400

    Added Gun and ammo forms

--- Gun Catalog/frmMain.Designer.vb
+++ Gun Catalog/frmMain.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,67 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnViewInventory = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnViewInventory
+        '
+        Me.btnViewInventory.Location = New System.Drawing.Point(117, 213)
+        Me.btnViewInventory.Name = "btnViewInventory"
+        Me.btnViewInventory.Size = New System.Drawing.Size(125, 31)
+        Me.btnViewInventory.TabIndex = 0
+        Me.btnViewInventory.Text = "View Inventory"
+        Me.btnViewInventory.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(227, 114)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(97, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Add Ammo"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(47, 114)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Add Gun"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(28, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(308, 39)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Gould Gun Catalog"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(365, 320)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnViewInventory)
+        Me.Name = "frmMain"
+        Me.Text = "Gould Gun Catalog"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnViewInventory As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
 End Class
--- Gun Catalog/frmMain.vb
+++ Gun Catalog/frmMain.vb
@@ -1,3 +1,13 @@
-﻿Public Class Form1
+﻿Public Class frmMain
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        frmGun.ShowDialog()
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        frmAmmo.ShowDialog()
+    End Sub
+
+    Private Sub btnViewInventory_Click(sender As Object, e As EventArgs) Handles btnViewInventory.Click
+
+    End Sub
 End Class
